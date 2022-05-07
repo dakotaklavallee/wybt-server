9cde9282d45de510027248c1ed8a8f17261fc2d2
Dakota Lavallee <dakota.k.lavallee@gmail.com>
2022-05-07 16:21:14 -0500

    Trying some different returns

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ const productsRouter = require("./products/products.router");
 const usersRouter = require("./users/users.router");
 const surveysRouter = require("./surveys/surveys.router");
 const avatarsRouter = require("./avatars/avatars.router");
+const cors = require('cors');
 
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/products", productsRouter);
 app.use("/users", usersRouter);
--- src/surveys/surveys.service.js
+++ src/surveys/surveys.service.js
@@ -1,14 +1,29 @@
-const knex = require('../db/connection.js');
+const knex = require("../db/connection.js");
+const reduceProperties = require("../utils/reduce-properties");
 
-function list(){
-    return knex("surveys").select('*');
+const addProducts = reduceProperties("survey_id", {
+  product_id: ["products", null, "product_id"],
+  product_name: ["products", null, "product_name"],
+  product_description: ["products", null, "product_description"],
+  product_img: ["products", null, "product_img"],
+  said_yes: ["products", null, "said_yes"],
+  said_no: ["products", null, "said_no"],
+  created_at: ["products", null, "created_at"],
+  updated_at: ["products", null, "updated_at"],
+});
+
+function list() {
+  return knex("surveys as s")
+  .join("products as p", "s.survey_id", "p.survey_id")
+  .select("s.*", "p.*")
+  .then(addProducts);
 }
 
-function read(surveyId){
-    return knex("surveys").select('*').where({ survey_id: surveyId }).first();
+function read(surveyId) {
+  return knex("surveys").select("*").where({ survey_id: surveyId }).first();
 }
 
 module.exports = {
-    list,
-    read,
-}
+  list,
+  read,
+};
